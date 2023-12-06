a0493b576657d5732d1d051a43be1dbeab8635d4
Inna1004m <inna1004.m@gmail.com>
2023-12-06 15:58:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+var y = 123;
+let str = "123";
+const bool = true;
+console.log(bool);
+let num = 123; //число
+console.log('num' typeof (num))
+let string = "Михаил"; // строка
+console.log('string', typeof(string))
+let bool = true;
+console.log('bool', typeof(bool1)));
+let nath = null // спец. значение
+console.log('nath', typeof(nath));
+let und = undefined // спец. значение
+console.log('und', typeof(und));
+BigInt
+Symbol
+
+// сложнные типы данных
+Object
+const obj = {
+    val:23,
+    num:"234",
+    arr: [1.32.45],
+    name: {
+        lastname: "Egorov",
+        firstname: "Pavel",
+        age:34,
+    }
+};
+
+console.log('obj', typeof(obj))
+console.log('obj', obj)
+
+//сложные типы
+const Arr=[1,2, "jvsk", true, [1,3,4,5]] //массивы
+function obj(){} //функции
+Date; //Даты
+RegExp; //Регулярные выражения
+
+
+console.log(num == str) //true
+console.log(num === str) // False
+let perv = num === str;
+let numNew = +(str)
+console.log ('numNew', numNew)
+console.log ('numNew', Boolean(numNew))
+
+//логические операторы
+
+// || // или
+// && // и
+// ! // инвертор
+let a = 0;
+
+let sum = a + b;
+let c = a * b;
+let d = a / b;
+let y = a - b;
+let u = a % b; // получения остатка от деления
+
+let a1 = a + i;
+
+// a = a + i
+a += i; 
+
+// операторы сравнения
+let sum1 = a < b;
+let c1 = a > b;
+let d1 = a <= b;
+let y1 = a >= b;
+let u1 = a != b; // не равно
+
+// числовые операторы
+// целое число = 67
+// дробное число = 6.7
+// экспон-ая = 5е5
+// бинарные = 1001 = 9;
+// восьмиричная = 0b111;
+// HEX
+// NAN - значение не является числом
+
+
+// операторы условий
+if(a>b) {
+    "Галина получит сертификат"
+} else if (a<b){
+    "Сертификат останется моим"
+} else if (a>c){
+
+} else {
+
+}
+
+// тернарный оператор
+let c4 = a>b ? 5 : 4;
+console.log('4', c4)
+
+let c4 = 5;
+// циклы for
+let num2 = 0;
+for(let i = 0; i>c4;i++) {
+    console.log(num2=i)
+}
+console.log(nym2)
+
+hello ("hello");
+//decloration функции
+
+function hello(a){
+    console.log(a);
+}
+
+
+//expression function
+
+const foo = function (b){
+    console.log(b)
+}
+foo("hi")
+
